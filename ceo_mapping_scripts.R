50c76ba81175ce44d34c4d2818b584b2226df98d
Dave Drennan <38985190+davedrenn@users.noreply.github.com>
2024-12-31 11:00:55 -0500

    Update ceo_mapping_scripts.R
    change to raw github filepaths

--- ceo_mapping_scripts.R
+++ ceo_mapping_scripts.R
@@ -1,3 +1,3 @@
-source("./scripts/basemap_ceo.R")
-source("./scripts/phl_ais_Rfunction.R")
-source("./scripts/theme_ceo.R")
+source("https://raw.githubusercontent.com/PHL-CEO/map_defaults/refs/heads/main/scripts/basemap_ceo.R")
+source("https://raw.githubusercontent.com/PHL-CEO/map_defaults/refs/heads/main/scripts/phl_ais_Rfunction.R")
+source("https://raw.githubusercontent.com/PHL-CEO/map_defaults/refs/heads/main/scripts/theme_ceo.R")
